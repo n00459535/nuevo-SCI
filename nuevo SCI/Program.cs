d0d958c1e146fd33b3d569ac661ebc2b84d9c393
LENOVO <LENOVO@DESKTOP-TPD3GVP>
2025-06-14 17:02:27 -0500

    Agregar archivos de proyecto.

---
+++ nuevo SCI/Program.cs
@@ -0,0 +1,17 @@
+﻿// Sistema de Monitoreo y Simulación Contra Incendios
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using MetodosSCI;
+namespace SistemaContraIncendios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuPrincipal.MostrarMenu();
+        }
+    }
+}
+
